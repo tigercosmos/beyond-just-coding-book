771779f8b1d551bc7e3d21f9bbc69ff82b684f15
anchi.liu <anchi.liu@mujin.co.jp>
2024-02-12 16:49:16 +0900

    update

--- ch3/3-3/perf-example/unsort.cpp
+++ ch3/3-3/perf-example/unsort.cpp
@@ -1,9 +1,14 @@
 #include <algorithm>
 #include <ctime>
 #include <iostream>
+#include <chrono>
+
+#define LOOP_NUM 3000
 
 int main()
 {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
     // 測試用陣列
     const int arr_len = 32768;
     int data[arr_len];
@@ -11,11 +16,11 @@ int main()
     for (int c = 0; c < arr_len; ++c)
         data[c] = std::rand() % 256;
 
-    std::sort(data, data + arr_len); // 進行排序
+    // 不進行排序
 
     long long sum = 0;
 
-    for (int i = 0; i < 30000; ++i)
+    for (int i = 0; i < LOOP_NUM; ++i)
     {
         for (int c = 0; c < arr_len; ++c)
         {
@@ -27,4 +32,6 @@ int main()
     }
 
     std::cout << "sum = " << sum << std::endl;
+    std::chrono::duration<double> elapsedTime = std::chrono::high_resolution_clock::now() - startTime;
+    std::cout << "elapsed time = " << elapsedTime.count() << " seconds" << std::endl;
 }
