d885f2ba43695b6d6dda1784e657f30adb4cd34a
tigercosmos <phy.tiger@gmail.com>
2024-07-19 02:57:24 +0900

    update

---
+++ ch5/5-1/cpp-unit-test/test_application.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// Database interface
+class Database
+{
+public:
+    virtual ~Database() {}
+    virtual bool connect(const std::string &url) = 0;
+    virtual bool disconnect() = 0;
+    virtual int query(const std::string &sql) = 0;
+};
+
+// Mock class
+class MockDatabase : public Database
+{
+public:
+    MOCK_METHOD(bool, connect, (const std::string &url), (override));
+    MOCK_METHOD(bool, disconnect, (), (override));
+    MOCK_METHOD(int, query, (const std::string &sql), (override));
+};
+
+// Class under test
+class Application
+{
+public:
+    Application(Database *db) : db_(db) {}
+
+    bool initialize(const std::string &url)
+    {
+        return db_->connect(url);
+    }
+
+    int executeQuery(const std::string &sql)
+    {
+        return db_->query(sql);
+    }
+
+private:
+    Database *db_;
+};
+
+// Tests
+TEST(ApplicationTest, InitializeTest)
+{
+    MockDatabase mock_db;
+
+    // Set expectation
+    EXPECT_CALL(mock_db, connect("localhost"))
+        .Times(1)
+        .WillOnce(testing::Return(true));
+
+    Application app(&mock_db);
+    EXPECT_TRUE(app.initialize("localhost"));
+}
+
+TEST(ApplicationTest, ExecuteQueryTest)
+{
+    MockDatabase mock_db;
+
+    // Set expectation
+    EXPECT_CALL(mock_db, query(testing::_))
+        .Times(1)
+        .WillOnce(testing::Return(42));
+
+    Application app(&mock_db);
+    EXPECT_EQ(app.executeQuery("SELECT SUM(Quantity) FROM OrderDetails;"), 42);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
