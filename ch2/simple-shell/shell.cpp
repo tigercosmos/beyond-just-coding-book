6d068a0879f1a0028d8537e75c62eadd452480d8
tigercosmos <phy.tiger@gmail.com>
2025-02-15 23:10:53 +0900

    fix simple shell comment

--- ch2/simple-shell/shell.cpp
+++ ch2/simple-shell/shell.cpp
@@ -12,10 +12,10 @@
 int main(int argc, char **argv)
 {
     /* 步驟一 */
-    // 模擬剛開啟 Shell 的顯示提示符號
+    // 模擬剛開啟 shell 的顯示提示符號
     std::cout << "$" << std::endl;
 
-    // 處理 SIGCHLD，可以避免 Child 疆屍程序
+    // 處理 SIGCHLD，可以避免 child 疆屍程序
     struct sigaction sigchld_action;
     sigchld_action.sa_handler = SIG_DFL,
     sigchld_action.sa_flags = SA_NOCLDWAIT;
@@ -24,9 +24,9 @@ int main(int argc, char **argv)
     // 模擬下達指令 ls | cat | cat | cat | cat | cat | cat | cat | cat
     std::cout << "$ ls | cat | cat | cat | cat | cat | cat | cat | cat" << std::endl;
 
-    // Shell 會需要 Parse 指令，我們假設 Shell 已經將指令 Parse 好
+    // shell 會需要 parse 指令，我們假設 shell 已經將指令 parse 好
 
-    // 接下來 Shell 的邏輯是，每一個指令都 fork 一個新的 Process，然後將 pipe 的讀取端或寫入端取代成 stdin 或 stdout
+    // 接下來 shell 的邏輯是，每一個指令都 fork 一個新的 process，然後將 pipe 的讀取端或寫入端取代成 stdin 或 stdout
 
     const char *ls_cmd = "ls";
     const char *cat_cmd = "cat";
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 
     const char *p2_args[] = {cat_cmd, NULL};
 
-    // 只是 DEMO，所以重複利用，真實世界 Shell 會一個一個 Parse
+    // 只是 DEMO，所以重複利用，真實世界 shell 會一個一個 parse
     for (int i = 1; i < COMMAND_COUNT; i++)
         cmds[i] = const_cast<char **>(p2_args);
 
@@ -52,42 +52,42 @@ int main(int argc, char **argv)
     {
 
         pid = fork();
-        if (pid == 0) // Child
+        if (pid == 0) // child
         {
             if (i != 0)
             {
-                // 用 dup2 將 pipe 輸出端複製成 stdin (fd=0)
+                // 用 dup2 將 pipe 的 read end 複製成 stdin (fd=0)
                 dup2(pipe_fds[(i - 1) * 2], STDIN_FILENO);
             }
 
             if (i != COMMAND_COUNT - 1)
             {
-                // 用 dup2 將 pipe 讀取端複製成 stdout (fd=1)
+                // 用 dup2 將 pipe 的 write end 複製成 stdout (fd=1)
                 dup2(pipe_fds[i * 2 + 1], STDOUT_FILENO);
             }
 
-            // 關閉 Pipe 開啟的 fd，因為此時 Pipe 兩端已經複製成 stdin 或 stdout
-            // Parent 跟 Child 都要關閉，避免其中一邊還開著 fd 導致程序無法結束
+            // 關閉 pipe 開啟的 fd，因為此時 pipe 兩端已經複製成 stdin 或 stdout
+            // parent 跟 child 都要關閉，避免其中一邊還開著 fd 導致程序無法結束
             if (i != 0)
             {
-                close(pipe_fds[(i - 1) * 2]);     // 前一個的寫
-                close(pipe_fds[(i - 1) * 2 + 1]); // 當前的讀
+                close(pipe_fds[(i - 1) * 2]);     // 前一個命令與當前命令連接的 pipe 的 read end
+                close(pipe_fds[(i - 1) * 2 + 1]); // 前一個命令與當前命令連接的 pipe 的 write end
             }
 
-            execvp(*cmds[i], cmds[i]); // 屬於 exec 系列的函數，會取代目前的 Process
+            execvp(*cmds[i], cmds[i]); // 屬於 exec 系列的函數，會取代目前的 process
 
             // execvp 正確執行的話，程式不會繼續到這裡
             fprintf(stderr, "Cannot run %s\n", *cmds[i]);
         }
-        else // Parent
+        else // parent
         {
             // printf("- fork %d\n", pid);
 
-            // 關閉開啟的 Pipe，Parent 跟 Child 都要關閉，避免其中一邊還開著 fd 導致程序無法結束
+            // 關閉開啟的 Pipe，Parent 跟 child 都要關閉，避免其中一邊還開著 fd 導致程序無法結束
             if (i != 0)
             {
-                close(pipe_fds[(i - 1) * 2]);     // 前一個的寫
-                close(pipe_fds[(i - 1) * 2 + 1]); // 當前的讀
+                close(pipe_fds[(i - 1) * 2]);     // 前一個命令與當前命令連接的 pipe 的 read end
+                close(pipe_fds[(i - 1) * 2 + 1]); // 前一個命令與當前命令連接的 pipe 的 write end
             }
         }
     }
@@ -95,6 +95,6 @@ int main(int argc, char **argv)
     waitpid(pid, NULL, 0); // 等最後一個指令結束
 
     /* 步驟三 */
-    // 模擬前一個命令結束之後，Shell 顯示提示符號
+    // 模擬前一個命令結束之後，shell 顯示提示符號
     std::cout << "$" << std::endl;
 }
